2e8633a250f4991bf4dce8e931b979f5b5a45fc4
SergeyGitAccount <l_w_d@i.ua>
2021-01-27 12:10:20 +0200

    initial commit

--- src/test/java/org/example/Lecture/LoginTest.java
+++ src/test/java/org/example/Lecture/LoginTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.Lecture;
 
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -7,7 +7,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
